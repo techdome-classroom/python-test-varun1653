014c06ed8bc614a21b39a9ab8e475848eb6159e9
varun1653 <109947079+varun1653@users.noreply.github.com>
2024-05-08 11:11:40 +0530

    Add files via upload

--- program1.py
+++ program1.py
@@ -1,6 +1,35 @@
 class Solution:
    
+
+    def explore_island(self, grid, row, col):
+        if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]) or grid[row][col] != "L":
+            return
+        grid[row][col] = "W"  
+
+        
+        self.explore_island(grid, row + 1, col)
+        self.explore_island(grid, row - 1, col)
+        self.explore_island(grid, row, col + 1)
+        self.explore_island(grid, row, col - 1)
+
     def getTotalIsles(self, grid: list[list[str]]) -> int:
-    #    write your code here
-                    
+        if not grid:
+            return 0
+
+        num_islands = 0
+        rows, cols = len(grid), len(grid[0])
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == "L":
+                    self.explore_island(grid, i, j)
+                    num_islands += 1
+
+        return num_islands
+
+
+
+
+         
         return 0
+        
