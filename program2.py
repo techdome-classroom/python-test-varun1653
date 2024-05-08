5116807b34d3e790b5d1c55dad3b6beef595fdd8
varun1653 <109947079+varun1653@users.noreply.github.com>
2024-05-08 11:19:21 +0530

    Add files via upload

--- program2.py
+++ program2.py
@@ -1,15 +1,3 @@
 
 
-def decode_message(s: str, p: str) -> bool:
-    if len(s) != len(p):
-        return False
-    
-    for m, k in zip(s, p):
-        if k == '*':
-            continue
-        elif k == '?':
-            continue
-        elif k != m:
-            return False
-    
-    return True
+
